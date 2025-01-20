0f18d70c7cb0193214f01c32f48a09c8efef5339
Bycollexion <your.email@example.com>
2025-01-20 23:06:30 +0800

    Add explicit host binding

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["gunicorn", "main:app"]
+CMD gunicorn --bind 0.0.0.0:$PORT main:app
--- main.py
+++ main.py
@@ -8,4 +8,5 @@ def index():
     return jsonify({"status": "healthy"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
